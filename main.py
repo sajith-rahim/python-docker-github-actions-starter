3141087025fb8de33685c91d593681a19d2834e8
sajith.rahim <sajith.rahim@outlook.com>
2023-03-24 01:38:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,15 +19,21 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: main.py <fname>")
         sys.exit(1)
-
-    fname = sys.argv[1]
-
+    
+    # Read Inputs
+    # You can read your input to your action from args or
+    # as env variables renamed as INPUT_<input_var_in_caps>
     print(f"Input: {sys.argv[1]} = {os.environ['INPUT_FNAME']}")
 
+
+    fname = sys.argv[1]
     print(f"Argument `fname` received is ${fname}")
+
     user = process(fname)
+    
     response = f"User {user.get('name')} is available at {user.get('website')}"
 
+    # Set Outputs
     # print(f"::set-output name=response-text::{response}") # ::set-output is soon to be deprecated
     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
         print(f'response-text={response}', file=fh)
